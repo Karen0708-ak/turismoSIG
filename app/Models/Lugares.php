b1c719b8905a45a1a41efb82c67bae33a22068c6
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-07 23:16:38 -0500

    Se agrego el modelo a la carpeta Models en el archivo Lugares.php

---
+++ app/Models/Lugares.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lugares extends Model
+{
+    //
+    use HasFactory;
+    //DEFINIR ATRIBUSTOS 
+    protected $fillable=[
+    'nombre',
+    'descripcion',
+    'categoria',
+    'imagen',
+    'latutud',
+    'longitud',
+];
+}
