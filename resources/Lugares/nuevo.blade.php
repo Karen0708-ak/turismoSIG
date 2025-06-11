f0650372121a156b6e4693d94ca7848088356f50
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-06-11 16:52:01 -0500

    Se creo el controlador Lugares y tambien se agrego la funcion index

---
+++ resources/Lugares/nuevo.blade.php
@@ -0,0 +1,57 @@
+@extends('layout.app')
+@section('contenido')
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6 bg-black p-4 rounded text-white">
+        <form action="{{ route('lugares.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <h1>Registrar Nuevo Lugar</h1>
+
+            <label for="nombre"><b>Nombre:</b></label>
+            <input class="form-control" type="text" name="nombre" id="nombre" required><br>
+
+            <label for="descripcion"><b>Descripción:</b></label>
+            <input class="form-control" type="text" name="descripcion" id="descripcion" required><br>
+
+            <label for="categoria"><b>Categoría:</b></label>
+            <input class="form-control" type="text" name="categoria" id="categoria" required><br>
+
+            <label for="imagen"><b>Imagen:</b></label>
+            <input class="form-control" type="file" name="imagen" id="imagen" accept="image/*"><br>
+
+            <label for="latitud"><b>Latitud:</b></label>
+            <input class="form-control" readonly type="text" name="latitud" id="latitud"><br>
+
+            <label for="longitud"><b>Longitud:</b></label>
+            <input class="form-control" readonly type="text" name="longitud" id="longitud"><br>
+
+            <div id="mapa_cliente" style="border:1px solid black; height:350px; width:100%"></div><br>
+
+            <button class='btn btn-success' type="submit">Guardar</button>
+            <a class="btn btn-danger" href="{{ route('lugares.index') }}">Cancelar</a>
+        </form>
+    </div>
+    <div class="col-md-3"></div>
+</div>
+
+<script type="text/javascript">
+    function initMap() {
+        var latitud_longitud = new google.maps.LatLng(-0.9374805, -78.6161327);
+        var mapa = new google.maps.Map(document.getElementById('mapa_cliente'), {
+            center: latitud_longitud,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var marcador = new google.maps.Marker({
+            position: latitud_longitud,
+            map: mapa,
+            title: "Seleccione la dirección",
+            draggable: true
+        });
+        google.maps.event.addListener(marcador, 'dragend', function (event) {
+            document.getElementById("latitud").value = this.getPosition().lat();
+            document.getElementById("longitud").value = this.getPosition().lng();
+        });
+    }
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LugaresController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/lugares/mapa',[LugaresController::class,'mapa']);
+//Habilitando acceso al controlador
+Route::resource('lugares',LugaresController::class);
+
+
