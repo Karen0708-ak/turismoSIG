8d761f4f6d8bb46b63fce82bccb08d85b9eda8d9
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-06-11 22:37:44 -0500

    se agrego una descripcion a la vista

--- resources/views/Lugares/index.blade.php
+++ resources/views/Lugares/index.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
 <div class="container mt-4">
     <div class="d-flex justify-content-between align-items-center mb-4">
-        <h1>Puntos de Interés Turístico</h1>
+        <h1>Puntos de Interés Turísticos</h1>
         <div>
             <a href="{{ route('Lugares.mapa') }}" class="btn btn-primary">Ver Mapa</a>
             <a href="{{ route('Lugares.create') }}" class="btn btn-success">Agregar Nuevo Lugar</a>
