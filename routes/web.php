ce2001f60aad69459cfd6c95b9712e1139b48f9d
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-06-11 22:39:56 -0500

    se corrigio las rutas

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,8 @@ Route::get('/', function () {
     return redirect()->route('Lugares.index');
 });
 
-// Rutas para el CRUD de lugares
 Route::resource('Lugares', LugaresController::class);
 
-// Ruta para el mapa
 Route::get('/Lugares/mapa', [LugaresController::class, 'mapa'])->name('Lugares.mapa');
 
-// Ruta para el filtrado AJAX
 Route::get('/Lugares/filtrar', [LugaresController::class, 'filtrar'])->name('Lugares.filtrar');
